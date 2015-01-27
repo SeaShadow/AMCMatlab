d1d5d91d69fe579a014656f08e69228a0541cd7f
Konrad Zurcher <konny.seashadow@gmail.com>
2015-01-27 17:36:44 +1100

    Updated analysis files. Upgraded thrust curve plot.

--- SelfPropulsionAnalysis/analysis_spp_ccdott.m
+++ SelfPropulsionAnalysis/analysis_spp_ccdott.m
@@ -3,7 +3,7 @@
 %# ------------------------------------------------------------------------
 %#
 %# Author     :  K. Zürcher (Konrad.Zurcher@utas.edu.au)
-%# Date       :  January 26, 2015
+%# Date       :  January 27, 2015
 %#
 %# Test date  :  November 5 to November 18, 2013
 %# Facility   :  AMC, Towing Tank (TT)
